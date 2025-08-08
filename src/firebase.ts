d9c466edc8dca5f7c8df4108511f45fc100875fc
cherabear <sammysavage36@gmail.com>
2025-08-08 08:36:57 -0700

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
+  apiKey: "AIzaSyCmfAkYQS55UBIn8lphcd6Ejig2aqsKwkE",
+  authDomain: "analytics-sws.firebaseapp.com",
+  projectId: "analytics-sws",
+  storageBucket: "analytics-sws.firebasestorage.app",
+  messagingSenderId: "209517929426",
+  appId: "1:209517929426:web:bc1078d3ed359d57405e0b",
 };
 
 const app = initializeApp(firebaseConfig);
